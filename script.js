f8b81c3e0e6089d7b5ead52ec3e180492b0810b3
Westfal <77891009+SnrWestfal@users.noreply.github.com>
2022-07-14 20:47:10 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function newFact() {
-    var facts = ['Технологии, которые я изучил', 'Frontend<br> HTML CSS JavaScript NodeJS React TailwindCSS Figma', 'Backend<br> Python Django PHP RestAPI MySQL RabbitMQ'];
-    var randomFact = Math.floor(Math.random() * facts.length);
+    const facts = ['Технологии, которые я изучил', 'Frontend<br> HTML CSS JavaScript NodeJS React TailwindCSS Figma', 'Backend<br> Python Django PHP RestAPI MySQL RabbitMQ'];
+    const randomFact = Math.floor(Math.random() * facts.length);
     document.getElementById('factDisplay').innerHTML = facts[randomFact];
   })();
