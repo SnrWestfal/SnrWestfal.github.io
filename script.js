4408cb130b6305ced1d914d8cb9eb9066dde0110
Westfal <77891009+SnrWestfal@users.noreply.github.com>
2022-09-07 15:09:57 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function newSkill() {
-    const skills = ['Технологии, которые я изучил', 'Frontend<br> HTML CSS JavaScript NodeJS React TailwindCSS Figma'];
+    const skills = ['Технологии, которые я изучил <br> обнови страницу :)', 'HTML CSS JavaScript JQuery PHP MySQL'];
     const randomFact = Math.floor(Math.random() * skills.length);
     document.getElementById('factDisplay').innerHTML = skills[randomFact];
   })();
